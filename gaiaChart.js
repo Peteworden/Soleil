2a180105243243c58b54e8182993af22b3caeaa0
Peteworden <111156550+Peteworden@users.noreply.github.com>
2024-06-07 10:22:09 +0900

    Add files via upload

--- gaiaChart.js
+++ gaiaChart.js
@@ -35,19 +35,24 @@ document.getElementById('getFile').addEventListener('change', function () {
         decmin = decArray.reduce(aryMin);
         magmax = magArray.reduce(aryMax);
         document.getElementById('magmaxSlider').max = magmax;
+        document.getElementById('magmaxSlider').min = magmax - 6;
         document.getElementById('magmaxSlider').value = magmax;
         document.getElementById('raminSlider').min = ramin;
         document.getElementById('raminSlider').max = ramax;
+        document.getElementById('raminSlider').step = (ramax - ramin) / 20;
         document.getElementById('raminSlider').value = ramin;
         document.getElementById('ramaxSlider').min = ramin;
         document.getElementById('ramaxSlider').max = ramax;
+        document.getElementById('ramaxSlider').step = (ramax - ramin) / 20;
         document.getElementById('ramaxSlider').value = ramax;
         document.getElementById('decminSlider').min = decmin;
         document.getElementById('decminSlider').max = decmax;
         document.getElementById('decminSlider').value = decmin;
+        document.getElementById('decminSlider').step = (decmax - decmin) / 20;
         document.getElementById('decmaxSlider').min = decmin;
         document.getElementById('decmaxSlider').max = decmax;
         document.getElementById('decmaxSlider').value = decmax;
+        document.getElementById('decmaxSlider').step = (decmax - decmin) / 20;
         draw(ramax, ramin, decmax, decmin, magmax, 1.6, 0.2);
     }
     fr.readAsText(this.files[0]);
@@ -86,9 +91,8 @@ document.getElementById('bSlider').addEventListener('input', function(){
 });
 
 function draw (ramax, ramin, decmax, decmin, magmax, a, b) {
-    console.log(ramax, ramin, decmax, decmin, magmax, a, b);
     document.getElementById('info').innerHTML = `R.A.:${Math.round(ramin*1000)/1000}~${Math.round(ramax*1000)/1000}deg, Dec:${Math.round(decmin*1000)/1000}~${Math.round(decmax*1000)/1000}deg, mag:~${Math.round(magmax*100)/100}mag`;
-    canvas.width = canvas.height * (ramax - ramin) / (decmax - decmin);
+    canvas.width = canvas.height * (ramax - ramin) * Math.cos((decmin + decmax) * Math.PI / 180 / 2)/ (decmax - decmin);
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -98,14 +102,33 @@ function draw (ramax, ramin, decmax, decmin, magmax, a, b) {
         var size = mag2size(magArray[i], a, b);
         ctx.beginPath();
         ctx.arc(x, y, size, 0, 2*Math.PI, false);
-        ctx.fill()
-    }
+        ctx.fill();
+    };
 
     function radec2xy (RA, Dec) {
         return [canvas.width * (ramax - RA) / (ramax - ramin), canvas.height * (decmax - Dec) / (decmax - decmin)];
     }
 
     function mag2size (mag, a, b) {
-        return Math.pow(magmax + 0.2 - mag, a) * b;
+        return Math.pow(magmax - mag, a) * b + 1;
     }
 }
+
+ctx.fillStyle = 'black'
+var ra_demo = [-0.5, 0, 0.5, -1.5, 1.5, -1.5, 1.5];
+var dec_demo = [0, 0, 0, 2, 2, -2, -2];
+var mag_demo = [10, 10, 10, 10, 6, 6, 10];
+document.getElementById('info').innerHTML = `R.A.:0~0deg, Dec:0~0deg, mag:~0mag`;
+canvas.width = canvas.height;
+ctx.clearRect(0,0,canvas.width,canvas.height);
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = 'white';
+for (var i=0; i<7; i++) {
+    var [x, y] = [canvas.width * ((3 - ra_demo[i]) / 6), canvas.height * (3 -dec_demo[i]) / 6];
+    var size = Math.pow(15 - mag_demo[i], 1.6) * 0.2 + 1;
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, 2*Math.PI, false);
+    ctx.fill();
+};
+
