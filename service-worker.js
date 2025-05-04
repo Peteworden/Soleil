d66f537d47afff2c01ac20fc560a6de33ee1e665
Peteworden <111156550+Peteworden@users.noreply.github.com>
2025-05-04 21:16:27 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'mobile-reticle-star-atlas-v1';
 const FILES_TO_CACHE = [
   '/Soleil/chart.html',
-  '/Soleil/chart.js',
+  '/Soleil/chart.js?v=2505042116',
   '/Soleil/styles/styleChart.css',
   '/Soleil/manifest.json',
   '/Soleil/images/icon/android-chrome-192x192.png',
