1320bb189b00858d2c3dde3e80e3d32140a04cca
unknown <masa27791ke@gmail.com>
2025-06-21 00:09:45 +0900

    update dataloader.js

--- dist/utils/DataLoader.js
+++ dist/utils/DataLoader.js
@@ -23,19 +23,19 @@ export class DataLoader {
     }
     // 星座データの読み込み
     static async loadConstellationData() {
-        return await this.fetchJson('/data/constellation.json');
+        return await this.fetchJson('data/constellation.json');
     }
     // メシエ天体データの読み込み
     static async loadMessierData() {
-        return await this.fetchJson('/data/messier.json');
+        return await this.fetchJson('data/messier.json');
     }
     // 星名データの読み込み
     static async loadStarNames() {
-        return await this.fetchJson('/data/starnames.json');
+        return await this.fetchJson('data/starnames.json');
     }
     // HIP星表データの読み込み
     static async loadHIPData() {
-        const h = await this.fetchText('/data/hip_65.txt');
+        const h = await this.fetchText('data/hip_65.txt');
         const hipData = h.split(',').map(Number);
         const hips = [];
         for (let i = 0; i < hipData.length; i += 4) {
@@ -51,23 +51,23 @@ export class DataLoader {
     }
     // NGC天体データの読み込み
     static async loadNGCData() {
-        return await this.fetchText('/data/ngc.txt');
+        return await this.fetchText('data/ngc.txt');
     }
     // 明るい星データの読み込み
     static async loadBrightStars() {
-        return await this.fetchText('/data/brights.txt');
+        return await this.fetchText('data/brights.txt');
     }
     // 星座境界データの読み込み
     static async loadConstellationBoundaries() {
-        return await this.fetchBinary('/data/constellation_boundaries.bin');
+        return await this.fetchBinary('data/constellation_boundaries.bin');
     }
     // Gaiaデータの読み込み
     static async loadGaiaData(region) {
-        return await this.fetchBinary(`/data/gaia_${region}.bin`);
+        return await this.fetchBinary(`data/gaia_${region}.bin`);
     }
     // 追加天体データの読み込み
     static async loadAdditionalObjects() {
-        return await this.fetchText('/data/additional_objects.txt');
+        return await this.fetchText('data/additional_objects.txt');
     }
 }
 //# sourceMappingURL=DataLoader.js.map
