a560eaa75571e8f6daf3065d5328988569d5df03
unknown <masa27791ke@gmail.com>
2025-07-22 19:16:43 +0900

    enabled to set the observation site with map

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mobile-reticle-star-atlas-v20250721';
+const CACHE_NAME = 'mobile-reticle-star-atlas-v20250722';
 const FILES_TO_CACHE = [
   // '/Soleil/styles/styleChart.css',
   // '/Soleil/manifest.json',
