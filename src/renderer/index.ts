d673092e938ddc3145ef59b33cd4c2a7121a9269
Masaru Sato <masa27791ke@gmail.com>
2026-05-16 12:42:51 +0900

    comment out renderer.index.ts

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,17 +1,5 @@
-import { StarChartConfig } from '../types/index.js';
-import { CanvasRenderer } from './CanvasRenderer.js';
-import { WebGLRenderer } from './webgl/webglRenderer.js';
+// import { StarChartConfig } from '../types/index.js';
+// import { CanvasRenderer } from './CanvasRenderer.js';
+// import { WebGLRenderer } from './webgl/webglRenderer.js';
 
-export type RendererType = 'canvas' | 'webgl';
-
-export function createRenderer(type: RendererType, canvas: HTMLCanvasElement, config: StarChartConfig) {
-    if (type === 'webgl') {
-        try {
-            return new WebGLRenderer(canvas, config);
-        } catch (_) {
-            // Fallback to Canvas when WebGL2 is unavailable
-            return new CanvasRenderer(canvas);
-        }
-    }
-    return new CanvasRenderer(canvas)
-}
+// export type RendererType = 'canvas' | 'webgl';
